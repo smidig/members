2c5e4c81e5103b93543f66b246a58f0dee202088
Ole Morten Amundsen <ole.morten.amundsen@gmail.com>
2012-02-20 01:22:24 +0100

    sent a bunch of invite emails. To tired to remember what I've been doing last hours

--- README.md
+++ README.md
@@ -2,19 +2,31 @@
 
 App to help inviting members and to coordinate general meetings
 
-## Setup and installation
+## Invite members
+
+to test locally:
+Member.invite!(:email => "ole.morten.amundsen@gmail.com", :name => "Ole Morten")
+
+NB: do not use this for production as invitation tokens will be store locally.
+
+TODO: create invitation sending at BackOffice or for other admin page.
+
+# Setup and installation
 
 See README at https://github.com/rubykurs/bootstrap for more info on how this app was built and started out. This app is based off on that and it is more likely to be updated than 'yet another app'
 
-# start
+## start
 bundle install  #NB: jeg opplever noen rare ting med at executablen ikke blir registrert etter gem install bundler og bundle. Gjør da `cd ..` og `cd members`, det har av snodige grunner funkert oss meg.
 
 rake db:migrate
-rake test
+rake test # probably a bad idea...
 
 rake db:seed #Loads what's in db/seeds.rb
 rails s #start server
 
+Notice the admin at the top right? Hint: look at db/seeds.rb - this is awesome!
 
-# Server
+
+
+## Server
 Deployed at heroku. Ask Knut Johannes Dahle or Ole Morten Heggebakken Amundsen for access.
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,11 @@
 # -*- encoding : utf-8 -*-
+require 'ostruct'
+if File.exists?(mailer_yml = "#{Rails.root}/config/mailer.yml")
+  options = YAML.load_file(mailer_yml)[Rails.env]
+  ActionMailer::Base.smtp_settings = OpenStruct.new(options).smtp_settings
+end
+
+
 Members::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
@@ -13,9 +20,6 @@ Members::Application.configure do
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
-
-  # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
@@ -37,6 +41,9 @@ Members::Application.configure do
   config.assets.debug = false
 
   #Configuration for devise
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp #:sendmail
+
 end
